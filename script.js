5647a97aedda43c554c836f9cd0d80f6c01cbbba
mintaekim <kmintae33@gmail.com>
2023-09-20 21:34:35 +0800

    set up logic

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-// const squares = new Array(9);
-
-// squares.forEach(square => {
-//     let div = document.createElement('div');
-// })
-
-// const board = document.querySelector('#board');
-// let div = document.createElement('div');
-// board.appendChild(div);
-
+// Creating the board cells
+let board = document.getElementById('board');
+for (let i = 0; i < 9; i++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    board.appendChild(cell);
+}
 
 // constants
 const colors = {
@@ -28,57 +25,42 @@ const winningCombo = [
 ];
 
 // state variables
-let boardArray = [[0,1,2],[3,4,5],[6,7,8]];
-let boardArray2d = [0,1,2,3,4,5,6,7,8];
-let turn = [1, -1];
-let winner = ['win', 'tie', 'playing'];
+let boardArray = [null,null,null,null,null,null,null,null,null];
+let turn = 1;
+let winner = 'Playing';
 
 // cached elements
-let board = document.getElementById('board');
-const cell = document.querySelector('.cell');
+// const cell = document.querySelector('.cell');
 const cells = Array.from(document.querySelectorAll('.cell')); // or [...document.querySelectorAll('.cell')];
-// const cell1 = document.querySelector('.cell1');
-// const cell2 = document.querySelector('.cell2');
-// const cell3 = document.querySelector('.cell3');
-// const cell4 = document.querySelector('.cell4');
-// const cell5 = document.querySelector('.cell5');
-// const cell6 = document.querySelector('.cell6');
-// const cell7 = document.querySelector('.cell7');
-// const cell8 = document.querySelector('.cell8');
-// const cell9 = document.querySelector('.cell9');
+const resetBtn = document.querySelector('.resetBtn');
 
 // functions
 function initialize() {
-    // how to map
-    boardArray = [[null,null,null],[null,null,null],[null,null,null]];
+    boardArray = [null,null,null,null,null,null,null,null,null]; //splice to update?
     turn = 1;
     winner = null;
-    renderBoard();
+    // renderBoard();
 }
 
 function renderBoard() {
-    boardArray.forEach((row, rowIdx) => {
-        row.forEach((cell, cellIdx) => {
-            boardArray[rowIdx][cellIdx].style.backgroundColor = colors[1];
-        }
-    )})
+    boardArray.forEach((cell, cellIdx) => {
+            boardArray[cellIdx].style.backgroundColor = colors[1];
+        })
 }
 
 function renderMessage() {
     if (winner !== null) {
-        winner = colors[1].toUpperCase();
+        console.log(colors[1].toUpperCase());
     } else if (winner == 'T') {
-        return 'Tie';
+        console.log('Tie');
     } else {
-        return `Congrats ${colors[1].toUpperCase()}!`;
+        console.log(`Congrats ${colors[1].toUpperCase()}!`);
     }
 }
 
-board.addEventListener('click', handleClick);
-// cell.addEventListener('click', handleClick); - why doesn't this work
+board.addEventListener('click', handleCellClick);
 
-/// let boardArray = [[0,1,2],[3,4,5],[6,7,8]];
-function handleClick(e) {
+function handleCellClick(e) {
     const selectedCell = e.target;
     // console.log(e);
 
@@ -89,16 +71,36 @@ function handleClick(e) {
     //     if (cell === selectedCell) console.log(idx);
     // })
 
-    console.log(boardArray)
-    if (boardArray2d[indexOfClickedCell] !== null) return;
-    if (winner !== null) return;
+    // console.log(boardArray)
+    if (boardArray[indexOfClickedCell] !== null) return;
+    if (winner !== null) return; // could be playing?
 
-    boardArray2d[indexOfClickedCell] = turn;
+    boardArray[indexOfClickedCell] = turn;
 
     turn = turn * -1
 
     winningCombo.forEach(combo => {
         // 3 board positions using the winning indices
-        boardArray2d[]
+        let total = boardArray[combo[0]] + boardArray[combo[1]] + boardArray[combo[2]];
+        // find absolute value of total
+        if (total < 0) total *= -1;
+        if (total === 3) {
+            winner = boardArray[combo[0]];
+            return; // or break?
+        }
+        // if there is no winner and there are no more nulls in the board
+        if (boardArray.indexOf(null) < 0) winner = 'T';
     })
+    renderBoard();
+    renderMessage();
+}
+
+resetBtn.addEventListener('click', handleReplayClick);
+
+function handleReplayClick(e) {
+    const reset = e.target;
+    console.log(reset)
+    // initialize();
+    // renderBoard();
+    // renderMessage();
 }
