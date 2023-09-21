ee5be7833dfb334a321b4cd4ff8851f2f6d79569
mintaek <kmt850@gmail.com>
2023-09-21 16:09:15 +0800

    edit

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ function initialize() {
 function renderBoard(e) {
     boardArray.forEach((cell, cellIdx) => {
             // boardArray[cellIdx].style.backgroundColor = colors[1];
+            // boardArray[cellIdx].style.backgroundColor = colors[cell]; - try this
             e.target.style.backgroundColor = colors[turn];
             // console.log(e.target)
         })
@@ -93,10 +94,12 @@ console.log('after:', turn);
     for (let combo of winningCombo) {
         // 3 board positions using the winning indices
         let total = boardArray[combo[0]] + boardArray[combo[1]] + boardArray[combo[2]];
+
         // find absolute value of total
 console.log(total)
         if (total < 0) total *= -1;
 console.log(total)
+
         if (total === 3) {
             winner = boardArray[combo[0]];
             return; // or break?
@@ -104,6 +107,7 @@ console.log(total)
         // if there is no winner and there are no more nulls in the board
         if (boardArray.indexOf(null) < 0) winner = 'T';
     }
+
     renderBoard(e);
     renderMessage();
 }
