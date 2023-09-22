e90162f35fe82b4b1022eefd2acb13ddd4ee465c
mintaek <kmt850@gmail.com>
2023-09-22 14:22:17 +0800

    clean up code and add comments

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let i = 0; i < 9; i++) {
     board.appendChild(cell);
 }
 
-// constants
+// Constants
 const colors = {
     'null': 'antiquewhite',
     '1': 'green',
@@ -25,38 +25,39 @@ const winningCombo = [
     [2,4,6]
 ];
 
-// state variables
+// State variables
 let boardArray, turn, winner;
-// flag = won/lost
 
-// cached elements
+// Cached elements
 const cells = Array.from(document.querySelectorAll('.cell')); // or [...document.querySelectorAll('.cell')];
 const resetBtn = document.querySelector('.resetBtn');
 const message = document.querySelector('.message');
 
-// functions
+// Functions
+
+// Initialize state variables
 function initialize() {
-    boardArray = [null,null,null,null,null,null,null,null,null]; //splice to update? -from yy
+    boardArray = [null,null,null,null,null,null,null,null,null];
     turn = 1;
     winner = null;
-    // add the event listener back after removing
+    // Add the event listener back after removing
     board.addEventListener('click', handleCellClick);
     renderMessage();
     renderBoard();
 }
 
+// Update the board display
 function renderBoard() {
     boardArray.forEach((cell, cellIdx) => {
-        // console.log(boardArray)
             // boardArray[cellIdx].style.backgroundColor = colors[1];
             // const cellElement = boardArray[boardArray.indexOf[e.target]]
             const cellElement = document.getElementById(`cell-${cellIdx}`);
-            // console.log(cellElement)
             cellElement.style.backgroundColor = colors[cell];
             // e.target.style.backgroundColor = colors[turn];
         })
 }
 
+// Update the message display
 function renderMessage() {
     // if (winner === null) {
     //     console.log('whose turn:', colors[turn].toUpperCase());
@@ -69,13 +70,10 @@ function renderMessage() {
     if (winner === null) message.innerHTML = `${colors[turn].toUpperCase()}'s Turn!`;
     else if (winner === 'T') message.innerHTML = "It's a tie";
     else { message.innerHTML = `Congrats, ${colors[turn].toUpperCase()}! You won!`};
-    // if (winner === 'T') console.log("TIE");
-    // else if (winner) console.log(`Congrats, ${colors[turn].toUpperCase()}! You won!`);
-    // else {console.log(`${colors[turn].toUpperCase()}'s Turn!`)};
 }
 
+// Add click event listener when the user clicks on a cell
 board.addEventListener('click', handleCellClick);
-
 function handleCellClick(e) {
     const selectedCell = e.target;
     const indexOfClickedCell = cells.indexOf(selectedCell);
@@ -87,83 +85,55 @@ function handleCellClick(e) {
     if (boardArray[indexOfClickedCell] !== null) return;
     if (winner !== null) return; 
 
-    // update board
+    // Change the selected board element to the player
     boardArray[indexOfClickedCell] = turn;
 
-    // renderBoard(e);
-
-    // update winner
+    // Render board and message display
     renderBoard();
     renderMessage();
-    // winner = checkWinner();
 
-
+    // Check for any winning combination
     for (let combo of winningCombo) {
         
-        // 3 board positions using the winning indices
+        // Sum up the player (1 or -1) for winning combo positions
         let total = boardArray[combo[0]] + boardArray[combo[1]] + boardArray[combo[2]];
 
-        // find absolute value of total
+        // Calculate the absolute value of total
         if (total < 0) total *= -1;
         if (total === 3) {
             winner = boardArray[combo[0]];
-            // remove listener to stop the game
+            // Remove click event listener to stop the game
             board.removeEventListener('click', handleCellClick);
+            // Update the message display to show the winner
             renderMessage();
             return; 
         }
-        // if there is no winner and there are no more nulls in the board
+        // Check when there is no winner and there are no more nulls in the board
         if (boardArray.indexOf(null) < 0) winner = 'T';
         // if (boardArray.includes(null)) winner = null;
     }
 
-        // update turn
+        // Swap turns
         turn = turn * -1
-
-        // check win state
-        // if (winner) {
-        //     board.removeEventListener('click', handleCellClick);
-        // }
-
-    // renderBoard();
-    // renderMessage();
-    // // if (winner) board.removeEventListener('click', handleCellClick);
 }
 
-// STOP when game is over!
-// function checkWinner() {
-//     // winningCombo.forEach(combo => {
-//         for (let combo of winningCombo) {
-//             // 3 board positions using the winning indices
-//             let total = boardArray[combo[0]] + boardArray[combo[1]] + boardArray[combo[2]];
-//             // find absolute value of total
-//             if (total < 0) total *= -1;
-//             if (total === 3) {
-//                 board.removeEventListener('click', handleCellClick);
-//                 return boardArray[combo[0]];
-//             }
-//             // if there is no winner and there are no more nulls in the board
-//             if (boardArray.indexOf(null) < 0) return 'T';
-//             // if there are null cells, still playing
-//             // if (boardArray.includes(null)) return null;
-//         }
-// }
-
+// Add click event listener for the reset button
 resetBtn.addEventListener('click', handleReplayClick);
 
+// Clear board and reset the game to the original state
 function handleReplayClick() {
-    // const reset = e.target;
-    // console.log(reset)
     clearBoard();
     initialize();
     renderMessage();
     renderBoard();
 }
 
+// Clear the colors in the board
 function clearBoard() {
     cells.forEach(cell => {
         cell.style.backgroundColor = '';
     })
 }
 
+// Initialize the game
 initialize();
